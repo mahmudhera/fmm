62c2d2b40fb5860d75d328018d4dcc6c7ed47d0a
Mahmudur <mahmudhera93@gmail.com>
2024-06-09 18:31:45 -0400

    use different calc

--- estimate_mut_rates.cpp
+++ estimate_mut_rates.cpp
@@ -19,6 +19,7 @@ the rates will be written.
 #include <random>
 #include <chrono>
 #include <set>
+#include <cassert>
 
 using namespace std;
 
@@ -152,9 +153,36 @@ tuple<double, double, double> estimate_mut_rates(int len_orig, int len_mut, int 
     cout << "fA_mut: " << fA_mut << endl;
     cout << "S: " << S << endl;
     cout << "D: " << D << endl;
+
+    double val1, val2;
+
+    val1 = 3.0 * (fA_mut - 1.0*L2/4.0) / (  (L-4.0*fA) * (1 + 3.0*D/(4.0*S))  );
+    val2 = 3.0 * (4.0 * S * fA_mut / (4.0*S + 3.0*D) - L2 * S / (4.0*S + 3.0*D) ) / (L - 4.0 * fA);
+
+    cout << val1 << " " << val2 << endl;
+
+    assert(val1 == val2);
+
+    val1 = 3.0 * (- fA + 1.0*L/4) / (  (L-4.0*fA) * (1 + 3.0*D/(4.0*S))  );
+    val2 = 3.0 * S / (4.0*S + 3.0*D);
+
+    cout << val1 << " " << val2 << endl;
+
+    assert(val1 == val2);
+
+    val1 = 3.0 * (fA_mut - fA + 1.0*L/4 - 1.0*L2/4) / (  (L-4.0*fA) * (1 + 3.0*D/(4.0*S))  );
+    val2 = 3.0 * (4.0 * S * fA_mut / (4.0*S + 3.0*D) - L2 * S / (4.0*S + 3.0*D) ) / (L - 4.0 * fA) 
+                        + 3.0 * S / (4.0*S + 3.0*D);
+
+    cout << val1 << " " << val2 << endl;
+    
+    // assert that the two values are equal up to 1e-6
+    assert(abs(val1 - val2) < 1e-6);
     
 
-    double subst_rate = 3.0 * (fA_mut - fA + 1.0*L/4 - 1.0*L2/4) / (  (L-4*fA) * (1 + 3*D/(4*S))  );
+    double subst_rate = 3.0 * (fA_mut - fA + 1.0*L/4 - 1.0*L2/4) / (  (L-4.0*fA) * (1 + 3.0*D/(4.0*S))  );
+    //double subst_rate = 3.0 * (4.0 * S * fA_mut / (4.0*S + 3.0*D) - L2 * S / (4.0*S + 3.0*D) ) / (L - 4.0 * fA) 
+    //                    + 3.0 * S / (4.0*S + 3.0*D);
     
     double del_rate = 1.0 * D * subst_rate / S;
 
