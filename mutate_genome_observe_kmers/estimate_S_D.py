a0886d1fed65fa55c3c1bfc990f23aa9c501887d
Mahmudur <mahmudhera93@gmail.com>
2024-06-23 07:56:50 -0400

    use similar code

--- mutate_genome_observe_kmers/estimate_S_D.py
+++ mutate_genome_observe_kmers/estimate_S_D.py
@@ -44,7 +44,7 @@ def estimate_S_D(genome_string, mutated_genome_string, k):
                         num_kmers_single_subst += 1
                         kmers_marked_for_subst.add(kmer_orig)
                     elif i == 0:
-                        if new_kmer not in mut_kmer_to_long_kmer_where_kmer_suffix or new_kmer not in orig_kmer_to_long_kmer_where_kmer_suffix:
+                        if new_kmer not in mut_kmer_to_long_kmer_where_kmer_suffix or kmer_orig not in orig_kmer_to_long_kmer_where_kmer_suffix:
                             continue
                         long_kmer_orig = orig_kmer_to_long_kmer_where_kmer_suffix[kmer_orig]
                         long_kmer_mut = mut_kmer_to_long_kmer_where_kmer_suffix[new_kmer]
@@ -52,7 +52,7 @@ def estimate_S_D(genome_string, mutated_genome_string, k):
                             num_kmers_single_subst += 1
                             kmers_marked_for_subst.add(kmer_orig)
                     else:
-                        if new_kmer not in mut_kmer_to_long_kmer_where_kmer_prefix or new_kmer not in orig_kmer_to_long_kmer_where_kmer_prefix:
+                        if new_kmer not in mut_kmer_to_long_kmer_where_kmer_prefix or kmer_orig not in orig_kmer_to_long_kmer_where_kmer_prefix:
                             continue
                         long_kmer_orig = orig_kmer_to_long_kmer_where_kmer_prefix[kmer_orig]
                         long_kmer_mut = mut_kmer_to_long_kmer_where_kmer_prefix[new_kmer]
@@ -62,6 +62,13 @@ def estimate_S_D(genome_string, mutated_genome_string, k):
 
     num_kmers_single_delt = 0
     k_plus_one_mers_orig = get_kmers(genome_string, k+1)
+    orig_k_plus_one_mer_where_it_is_suffix = {}
+    orig_k_plus_one_mer_where_it_is_prefix = {}
+
+    for long_kmer in long_kmers_orig:
+        orig_k_plus_one_mer_where_it_is_suffix[long_kmer[:k+1]] = long_kmer
+        orig_k_plus_one_mer_where_it_is_prefix[long_kmer[1:]] = long_kmer
+
     for k_plus_one_mer in k_plus_one_mers_orig:
         for i in range(k+1):
             new_kmer = k_plus_one_mer[:i] + k_plus_one_mer[i+1:]
@@ -70,6 +77,26 @@ def estimate_S_D(genome_string, mutated_genome_string, k):
             if new_kmer in kmers_mutated_set:
                 if i != 0 and i != k:
                     num_kmers_single_delt += 1
+                elif i == 0:
+                    if new_kmer not in mut_kmer_to_long_kmer_where_kmer_suffix:
+                        continue
+                    if k_plus_one_mer not in orig_k_plus_one_mer_where_it_is_suffix:
+                        continue
+                    long_kmer_mut = mut_kmer_to_long_kmer_where_kmer_suffix[new_kmer]
+                    long_kmer_orig = orig_k_plus_one_mer_where_it_is_suffix[k_plus_one_mer]
+
+                    if long_kmer_mut[0] == long_kmer_orig[0]:
+                        num_kmers_single_delt += 1
+                else:
+                    if new_kmer not in mut_kmer_to_long_kmer_where_kmer_prefix:
+                        continue
+                    if k_plus_one_mer not in orig_k_plus_one_mer_where_it_is_prefix:
+                        continue
+                    long_kmer_mut = mut_kmer_to_long_kmer_where_kmer_prefix[new_kmer]
+                    long_kmer_orig = orig_k_plus_one_mer_where_it_is_prefix[k_plus_one_mer]
+
+                    if long_kmer_mut[-1] == long_kmer_orig[-1]:
+                        num_kmers_single_delt += 1
 
     print(num_kmers_single_subst)
     print(num_kmers_single_delt)
