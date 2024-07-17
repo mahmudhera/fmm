5dce5e7f3b5f8a1075c7303bc9229815da26d0d2
Mahmudur <mahmudhera93@gmail.com>
2024-07-17 15:34:08 -0400

    modified to compute cuttlefish

--- using_edlib/mutate_ndl.py
+++ using_edlib/mutate_ndl.py
@@ -1,9 +1,11 @@
 from itertools import product
 from mutate_genome import mutate
 from tqdm import tqdm
+from run_cuttlefish import run_cuttlefish
 
-mutation_rates = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
+mutation_rates = [0.01, 0.02, 0.03, 0.04, 0.05]
 k = 21
-for ps, pd, d, seed in tqdm(list(product(mutation_rates, mutation_rates, mutation_rates, range(10)))):
+for ps, pd, d, seed in tqdm(list(product(mutation_rates, mutation_rates, mutation_rates, range(5)))):
     mutated_filename = "data/ndl_mutated_" + str(ps) + "_" + str(pd) + "_" + str(d) + "_" + str(seed) + ".fasta"
-    mutate("data/ndl.fasta", ps, pd, d, seed, mutated_filename, k)
+    cuttlefish_prefix = mutated_filename + '_unitigs'
+    run_cuttlefish(mutated_filename, k, 64, cuttlefish_prefix)
