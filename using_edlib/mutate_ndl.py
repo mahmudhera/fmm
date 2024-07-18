1bd1adbd51aa5df830b67a280597e76400feff94
Mahmudur <mahmudhera93@gmail.com>
2024-07-18 12:26:08 -0400

    reduce the num of samples

--- using_edlib/mutate_ndl.py
+++ using_edlib/mutate_ndl.py
@@ -2,8 +2,8 @@ from itertools import product
 from mutate_genome import mutate
 from tqdm import tqdm
 
-mutation_rates = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
+mutation_rates = [0.01, 0.02, 0.03, 0.04, 0.05]
 k = 21
-for ps, pd, d, seed in tqdm(list(product(mutation_rates, mutation_rates, mutation_rates, range(10)))):
+for ps, pd, d, seed in tqdm(list(product(mutation_rates, mutation_rates, mutation_rates, range(5)))):
     mutated_filename = "data/staphylococcus_mutated_" + str(ps) + "_" + str(pd) + "_" + str(d) + "_" + str(seed) + ".fasta"
     mutate("data/staphylococcus.fasta", ps, pd, d, seed, mutated_filename, k)
